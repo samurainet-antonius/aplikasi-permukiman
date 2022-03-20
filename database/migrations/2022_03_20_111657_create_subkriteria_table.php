206267198772dd74574d6e018d72dca817a0ccfa
antonius <antoniusardyyansah@gmail.com>
2022-03-20 18:44:02 +0700

    make migration kriteria & subkriteria

---
+++ database/migrations/2022_03_20_111657_create_subkriteria_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubkriteriaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subkriteria', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kriteria_id');
+            $table->string('nama');
+            $table->foreign('kriteria_id')->references('id')->on('kriteria')->onDelete('cascade');
+            $table->boolean('flag_pakai')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subkriteria');
+    }
+}
