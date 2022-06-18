f76e2910bcd4d2f527671348d22c3e534587e5a8
Antonius A - Samurai Net <antoniusardyyansah@gmail.com>
2022-06-18 19:52:13 +0700

    fix design evaluasi

--- resources/views/app/evaluasi/create.blade.php
+++ resources/views/app/evaluasi/create.blade.php
@@ -1,4 +1,21 @@
 <x-app-layout>
+    @push('styles')
+        <script src="{{ asset('/assets/vendor/jquery/jquery.min.js') }}"></script>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"/>
+        <link rel="stylesheet" href="https://labs.easyblog.it/maps/leaflet-search/src/leaflet-search.css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.css" />
+        <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css" />
+
+        <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"></script>
+        <script src="https://labs.easyblog.it/maps/leaflet-search/src/leaflet-search.js"></script>
+        <script src="https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js"></script>
+
+        <style>
+            #map {
+                height: 500px;
+            }
+        </style>
+    @endpush
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">@lang('crud.evaluasi.create_title')</h1>
             <ol class="breadcrumb">
