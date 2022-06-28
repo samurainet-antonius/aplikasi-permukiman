f5adc88a7d7d966fea0d258e699bf066239bf606
alvianrizky <alvianrizky62@gmail.com>
2022-06-28 15:40:11 +0700

    update route

--- resources/views/app/evaluasi/show.blade.php
+++ resources/views/app/evaluasi/show.blade.php
@@ -271,7 +271,7 @@
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form method="POST" action="{{ route('evaluasi.store.kriteria', ['evaluasi_id' => $evaluasi->id]) }}" has-files class="mt-4" enctype="multipart/form-data">
+            <form method="POST" action="{{ route('evaluasi.store.kriteria.edit', ['evaluasi_id' => $evaluasi->id]) }}" has-files class="mt-4" enctype="multipart/form-data">
                 <div class="modal-body">
 
                     @csrf
